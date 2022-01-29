2c24a390b9a70620736a5f9f33057efc35a84f37
EhtishamNazir <41062866+EhtishamNazir@users.noreply.github.com>
2022-01-29 09:18:55 +0500

    New note passed back to the app

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -19,12 +19,17 @@ function CreateArea(props) {
 
   }
 
+  function submitNote(event){
+    props.onAdd(note);
+    event.preventDefault();
+  }
+
   return (
     <div>
       <form>
         <input onChange={handleChange} value={note.title} name="title" placeholder="Title" />
         <textarea onChange={handleChange} value={note.content} name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
